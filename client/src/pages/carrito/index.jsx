7de49c2ed4809b7a5293bb2bf7d1a702526708f2
Priscila Elias <priscilaek@gmail.com>
2023-12-12 02:02:41 -0500

    carrito con useCart hook

--- client/src/pages/carrito/index.jsx
+++ client/src/pages/carrito/index.jsx
@@ -1,15 +1,77 @@
+import { useEffect, useContext } from "react"
+import { useCart } from "../../hooks/useCart"
+import { Link } from "react-router-dom"
+import UserContext from "../../context/User/UserContext"
+
 export default function Cart() {
+  const userCtx = useContext(UserContext)
+
+  const { cart, sessionURL, getCheckoutSession, editCart } = userCtx
+
+  const { total, handleSubmit, handleChange, handleRemove } = useCart(
+    cart,
+    getCheckoutSession,
+    editCart
+  )
+
+  useEffect(() => {
+    if (sessionURL) window.location.href = sessionURL
+  }, [sessionURL])
+
   return (
     <div>
       <div>
-        Tu carrito de compras tiene los siguientes productos:
-        <p>Consulta 1</p>
-        <p>Consulta 2</p>
-        <p>Consulta 3</p>
+        <ul>
+          {cart.map((e) => {
+            return (
+              <li key={e._id}>
+                <h3>
+                  <Link to={`/pizzas/${e.slug}`}>{e.name}</Link>
+                </h3>
+
+                <p>{e.size}</p>
+                <p>${((e.price / 100) * e.quantity).toFixed(2)}</p>
+                <select
+                  name={e.priceID}
+                  onChange={(e) => {
+                    handleChange(e)
+                  }}
+                >
+                  {Array(5)
+                    .fill(null)
+                    .map((_, i) => {
+                      const initial = i + 1
+                      return initial === e.quantity ? (
+                        <option selected value={initial}>
+                          {initial}
+                        </option>
+                      ) : (
+                        <option value={initial}>{initial}</option>
+                      )
+                    })}
+                </select>
+
+                <button
+                  type="button"
+                  onClick={(evt) => {
+                    handleRemove(evt, e.priceID)
+                  }}
+                >
+                  Eliminar
+                </button>
+              </li>
+            )
+          })}
+        </ul>
       </div>
 
       <div>
-        <button>Pagar carrito</button>
+        <p>Total</p>
+        <p>$ {total.toFixed(2)}</p>
+      </div>
+
+      <div>
+        <button onClick={handleSubmit}>Pagar carrito</button>
       </div>
     </div>
   )
