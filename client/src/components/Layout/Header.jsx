900b60f5a9a1b67a07fc31854b781c803f8e56b3
Priscila Elias <priscilaek@gmail.com>
2023-12-12 00:56:59 -0500

    total count

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -8,6 +8,7 @@ import { Helmet } from "react-helmet"
 //import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 function Header() {
+  const [total, setTotal] = useState(0)
   const [user, setUser] = useState({
     name: "",
     lastname: "",
@@ -15,8 +16,12 @@ function Header() {
 
   const userCtx = useContext(UserContext)
 
-  const { authStatus, currentUser, logoutUser } = userCtx
+  const { cart, authStatus, currentUser, logoutUser, getCart } = userCtx
 
+  console.log(cart)
+  console.log(getCart)
+
+//useEffect autenticacion
   useEffect(() => {
     if (currentUser) {
       const { name, lastname } = currentUser
@@ -27,6 +32,26 @@ function Header() {
       })
     }
   }, [currentUser])
+
+//useEffect de atualizacion de carrito
+  useEffect(() => {
+    const fetchCart = async () => {
+      await getCart()
+    }
+    fetchCart()
+  }, [currentUser])
+
+//useEffect de actualizacion de total - calculo del carrito
+  useEffect(() => {
+    const getTotalProducts = () => {
+      const totalQty = cart.reduce((acc, cv) => {
+        return acc + cv.quantity
+      }, 0)
+      return totalQty
+    }
+    const result = getTotalProducts()
+    setTotal(result)
+  }, [cart])
 
   return (
     <div className="p-4 md:p-8">
@@ -65,7 +90,7 @@ function Header() {
         </div>
         <p className="text-center md:text-right underline">
           <Link to="/carrito" className="flex items-center">
-            <Cart className="mr-2" /> Tu carrito de compras: <span> 2 Consultas </span>
+            <Cart className="mr-2" /> Tu carrito de compras: <span> {total} Consulta(s) </span>
           </Link>
         </p>
       </ul>
